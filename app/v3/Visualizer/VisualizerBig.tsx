6eee12530bd4e6f0ecb3d56dd42ad2a51b0e50bd
Hrishi Olickel <hrishioa@gmail.com>
2023-05-24 02:19:29 +0800

    Okay I went overboard

--- app/v3/Visualizer/VisualizerBig.tsx
+++ app/v3/Visualizer/VisualizerBig.tsx
@@ -3,6 +3,80 @@
 import useAudioStore from '@/store/audio';
 import React, { useEffect, useRef } from 'react';
 
+import {
+  Scene,
+  PerspectiveCamera,
+  WebGLRenderer,
+  BoxGeometry,
+  Mesh,
+  MeshBasicMaterial,
+  Color,
+} from 'three';
+
+function visualize3D(
+  canvas: HTMLCanvasElement,
+  width: number,
+  height: number,
+  analyser: AnalyserNode
+) {
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+
+  // Set up three.js scene
+  const scene = new Scene();
+  const camera = new PerspectiveCamera(90, width / height, 0.1, 1000);
+  camera.position.z = 15;
+  camera.position.y = 5;
+  camera.rotation.x = -0.45;
+
+  const renderer = new WebGLRenderer({ canvas: canvas, antialias: true });
+  renderer.setSize(width, height);
+
+  // Create an array of materials to use for the bars
+  const materials = Array.from(
+    { length: bufferLength },
+    (_, i) =>
+      new MeshBasicMaterial({
+        color: new Color(`hsl(${(i / bufferLength) * 360}, 100%, 50%)`),
+      })
+  );
+
+  // Create an array of geometry/mesh for the bars
+  const bars = Array.from({ length: bufferLength }, (_, i) => {
+    const geometry = new BoxGeometry(0.5, 0.5, 5);
+    const bar = new Mesh(geometry, materials[i]);
+    bar.position.x = (i / bufferLength) * 20 - 10; // Spread the bars along the x axis
+    scene.add(bar);
+    return bar;
+  });
+
+  function stop() {
+    // Clean up scene when stop is called
+    bars.forEach((bar) => scene.remove(bar));
+    renderer.dispose();
+  }
+
+  function draw() {
+    // Fetch the new data
+    analyser.getByteFrequencyData(dataArray);
+    // Update the scale of the bars to visualize the frequency
+    bars.forEach((bar, i) => {
+      const h = (dataArray[i] / 255) * 10; // Multiply by 10 for more dramatic heights
+      bar.scale.z = h;
+      bar.position.y = h / 2; // Move the bar up so it extends down from the original position
+    });
+
+    // Render the scene
+    renderer.render(scene, camera);
+
+    // Continue looping
+    requestAnimationFrame(draw);
+  }
+
+  draw();
+  return stop;
+}
+
 // Visualization functions
 const visualizeSineWave = (
   canvasCtx: CanvasRenderingContext2D,
@@ -11,7 +85,6 @@ const visualizeSineWave = (
   height: number,
   backgroundColor: string,
   strokeColor: string,
-  context: AudioContext,
   analyser: AnalyserNode
 ): (() => void) => {
   function hexToRgb(hex: string): { r: number; g: number; b: number } | null {
@@ -80,6 +153,7 @@ interface AudioVisualizerProps {
   height: number;
   backgroundColor: string;
   strokeColor: string;
+  three: boolean;
 }
 
 const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
@@ -87,14 +161,18 @@ const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
   height,
   backgroundColor,
   strokeColor,
+  three,
 }) => {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const stopVisualizer = useRef<(() => void) | null>(null);
   const recordingState = useAudioStore((state) => state.recordingState);
 
   useEffect(() => {
-    const startVisualizer = async () => {
-      try {
+    if (three) {
+      const canvas = canvasRef.current;
+
+      // Define async function inside useEffect
+      const startVisualizer = async () => {
         const stream = await navigator.mediaDevices.getUserMedia({
           audio: true,
           video: false,
@@ -104,40 +182,75 @@ const AudioVisualizer: React.FC<AudioVisualizerProps> = ({
         let analyser = audioContext.createAnalyser();
         source.connect(analyser);
 
-        if (canvasRef.current) {
-          const canvasCtx = canvasRef.current.getContext('2d');
-          if (canvasCtx) {
-            stopVisualizer.current = visualizeSineWave(
-              canvasCtx,
-              canvasRef.current,
-              width,
-              height,
-              backgroundColor,
-              strokeColor,
-              audioContext,
-              analyser
-            );
-          }
+        if (canvas) {
+          stopVisualizer.current = visualize3D(canvas, width, height, analyser);
         }
-      } catch (error) {
-        console.error('Error starting visualizer:', error);
-      }
-    };
+      };
 
-    if (recordingState === 'RECORDING') {
-      startVisualizer();
+      // Call async function
+      if (recordingState === 'RECORDING') {
+        startVisualizer();
+      } else {
+        if (stopVisualizer.current) {
+          stopVisualizer.current();
+        }
+      }
+
+      return () => {
+        if (stopVisualizer.current) {
+          stopVisualizer.current();
+        }
+      };
     } else {
-      if (stopVisualizer.current) {
-        stopVisualizer.current();
-      }
-    }
+      const startVisualizer = async () => {
+        try {
+          const stream = await navigator.mediaDevices.getUserMedia({
+            audio: true,
+            video: false,
+          });
+          const audioContext = new AudioContext();
+          const source = audioContext.createMediaStreamSource(stream);
+          let analyser = audioContext.createAnalyser();
+          source.connect(analyser);
 
-    return () => {
-      if (stopVisualizer.current) {
-        stopVisualizer.current();
+          if (canvasRef.current) {
+            const canvasCtx = canvasRef.current.getContext('2d');
+            if (canvasCtx) {
+              stopVisualizer.current = visualizeSineWave(
+                canvasCtx,
+                canvasRef.current,
+                width,
+                height,
+                backgroundColor,
+                strokeColor,
+                analyser
+              );
+            }
+          }
+        } catch (error) {
+          console.error('Error starting visualizer:', error);
+        }
+      };
+
+      if (recordingState === 'RECORDING') {
+        startVisualizer();
+      } else {
+        if (stopVisualizer.current) {
+          stopVisualizer.current();
+        }
       }
-    };
-  }, [backgroundColor, height, recordingState, strokeColor, width]);
+
+      return () => {
+        if (stopVisualizer.current) {
+          stopVisualizer.current();
+        }
+      };
+    }
+  }, [three, width, height, recordingState]);
+
+  // useEffect(() => {
+  //
+  // }, [backgroundColor, height, recordingState, strokeColor, width]);
 
   return <canvas ref={canvasRef} width={width} height={height} />;
 };
--- app/v3/page.tsx
+++ app/v3/page.tsx
@@ -17,10 +17,20 @@ export default function Home() {
     <main className={FiraSans.className + ' p-6 flex h-screen flex-col gap-5'}>
       <div className="text-2xl font-bold">Version 3, Albert!</div>
       <div className="flex justify-center">
-        <div style={{ width: '800px', height: '400px' }}>
+        <div style={{ width: '500px', height: '100px' }}>
           <AudioVisualizer
-            width={800}
-            height={400}
+            width={500}
+            height={100}
+            three={true}
+            backgroundColor="#000"
+            strokeColor="#fff"
+          />
+        </div>
+        <div style={{ width: '500px', height: '100px' }}>
+          <AudioVisualizer
+            width={500}
+            height={100}
+            three={false}
             backgroundColor="#000"
             strokeColor="#fff"
           />
